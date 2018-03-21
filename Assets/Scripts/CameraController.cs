8d7435e46618be0c4a20558045f2b501ef9f5244
Charlie <cdavenport090@gmail.com>
2018-03-21 17:07:53 -0400

    Added Camera Pivots
    user can now swap between camera pivots (position + rotation). To add a new pivot, just place a new empty gameObject under the Camera Pivots object, and the CameraController script will automatically add it to the list of pivots

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform pivots_gameobj;
+    //[SerializeField]
+    private List<Transform> cameraPivots;
+    private int currentPivot_ind;
+    private Transform currentPivot;
+    private bool transitioning; // prevent coroutine being called multiple times 
+
+    // initialize
+    private void Awake() {
+
+        cameraPivots = new List<Transform>();
+        currentPivot_ind = 0;
+        // get the transforms of all children under this gameObject
+        for (int i=0; i<pivots_gameobj.childCount; i++) {
+            cameraPivots.Add(pivots_gameobj.GetChild(i));
+        }
+        currentPivot = cameraPivots[currentPivot_ind];
+        this.transform.position = currentPivot.position;
+        this.transform.rotation = currentPivot.rotation;
+    }
+
+    // call this from other scripts to change camera view
+    public void nextPivot() {
+        
+        if (!transitioning) { // this if-statement prevents coroutine from being interrupted
+            currentPivot_ind = (currentPivot_ind + 1) % cameraPivots.Count; // next pivot (cycles back to first)
+            Transform nextPivot = cameraPivots[currentPivot_ind];
+            StartCoroutine(do_pivot(currentPivot, nextPivot));
+            currentPivot = nextPivot;
+        }
+
+    }
+
+    // coroutine
+    // updates camera position/rotation every fixed update
+    IEnumerator do_pivot(Transform start, Transform stop) {
+        transitioning = true;
+        int n_iter = 20;
+        float incr = 1f / (float)n_iter;
+        for (float t = 0; t < 1f; t += incr) {
+            this.transform.position = Vector3.Lerp(start.position, stop.position, t);
+            this.transform.rotation = Quaternion.Lerp(start.rotation, stop.rotation, t);
+            yield return new WaitForFixedUpdate();
+        }
+        transitioning = false; 
+    }
+
+
+}//CameraController
